e283eaf862408bc895b7dfcf0e50f77e44fcfccb
Hanter Jung <hanterkr@gmail.com>
2019-06-11 17:55:14 +0900

    Revert "Fix minHeight of gallery"
    This reverts commit e7d3c639b22d9d6aa5169ac26df416c6fdca80a2.

--- src/App.js
+++ src/App.js
@@ -25,130 +25,124 @@ import imgSnaps from './assets/img/snap'
 import "react-image-gallery/styles/css/image-gallery.css";
 import "./image-gallery-custom.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const App = () => {
+  const styles = useStyles();
+  const { height, width } = useWindowDimensions();
 
-  render() {
-    const styles = useStyles();
-    const {height, width} = useWindowDimensions();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md" className={styles.container}>
+        <Typography variant='h2' component='h2'
+                    className={classNames(styles.announcement, styles.fontSansSerif)}>
+          WEDDING ANNOUNCEMENT
+        </Typography>
 
-    return (
-      <React.Fragment>
-        <CssBaseline/>
-        <Container maxWidth="md" className={styles.container}>
-          <Typography variant='h2' component='h2'
-                      className={classNames(styles.announcement, styles.fontSansSerif)}>
-            WEDDING ANNOUNCEMENT
+        {width > height ? (
+          <CardMedia
+            className={styles.mainSnap}
+            image={imgSnapMain}
+            title="Main Snap"
+          />
+        ) : ( height > width*2 ? (
+          <CardMedia
+            className={styles.mainSnapMobileLong}
+            image={imgSnapMainMobile}
+            title="Main Snap (Mobile long ver.)"
+          />
+        ) : (
+          <CardMedia
+            className={styles.mainSnapMobile}
+            image={imgSnapMainMobile}
+            title="Main Snap (Mobile ver.)"
+          />
+        ))}
+
+        <div className={styles.introduce} >
+          <div className={styles.newlywedsContainer}>
+            <Typography variant='h4' component='span'
+                        className={classNames(styles.newlywedsName, styles.fontHandwriting)}>
+              한터</Typography>
+            <Typography variant='h5' component='span'
+                        className={classNames(styles.newlywedsHeart, styles.fontHandwriting)}>
+              &nbsp;♥&nbsp;</Typography>
+            <Typography variant='h4' component='span'
+                        className={classNames(styles.newlywedsName, styles.fontHandwriting)}>
+              수지</Typography>
+          </div>
+          <div className={styles.dateContainer}>
+            <Typography variant='h6' component='div'
+                        className={classNames(styles.date, styles.fontHandwriting)}>
+              2019.08.07. WED.
+            </Typography>
+          </div>
+        </div>
+
+        <div className={styles.details}>
+          <CardMedia
+            className={styles.detailFrameTop}
+            image={imgDetailTitleFrameTop}
+          />
+          <Typography variant='h3' component='h3' gutterBottom
+                      className={classNames(styles.detailTitle, styles.fontCursive)}>
+            Save the Date
+          </Typography>
+          <Typography variant='p' component='p' paragraph
+                      className={classNames(styles.detailContent, styles.fontSerif)}>
+            서로가 마주보며 다져온 사랑을 <br/>
+            이제 함께 한 곳을 바라보며 걸어갈 수 있는 <br/>
+            큰 사랑으로 키우고자 합니다. <br/>
+            <br/>
+            저희 두 사람이 사랑의 이름으로 지켜나갈 수 있게 <br/>
+            앞날을 축복해 주시면 감사하겠습니다.
+          </Typography>
+          <CardMedia
+            className={styles.detailSubSnap}
+            image={imgSnapSub}
+            title="Sub Snap"
+          />
+
+          <div className={styles.newlywedsContainer}>
+            <Typography variant='h4' component='div'
+                        className={classNames(styles.newlywedsName2, styles.fontSerif)}>
+              정한터&nbsp;</Typography>
+            <CardMedia
+              className={styles.newlywedsFlowerImg}
+              image={imgHeartFlower}
+              title="Heart"
+            />
+            <Typography variant='h4' component='div'
+                        className={classNames(styles.newlywedsName2, styles.fontSerif)}>
+              &nbsp;황수지</Typography>
+          </div>
+
+          <Typography variant='h6' component='div'
+                      className={classNames(styles.detailFamily, styles.fontSerif)}>
+            정문식 · 박희숙의 장남&nbsp;&nbsp;<strong className={styles.detailFamilyNewlywed}>한터</strong>
+            <br />
+            황용환 · 김윤희의 장녀&nbsp;&nbsp;<strong className={styles.detailFamilyNewlywed}>수지</strong>
           </Typography>
 
-          {width > height ? (
-            <CardMedia
-              className={styles.mainSnap}
-              image={imgSnapMain}
-              title="Main Snap"
-            />
-          ) : (height > width * 2 ? (
-            <CardMedia
-              className={styles.mainSnapMobileLong}
-              image={imgSnapMainMobile}
-              title="Main Snap (Mobile long ver.)"
-            />
-          ) : (
-            <CardMedia
-              className={styles.mainSnapMobile}
-              image={imgSnapMainMobile}
-              title="Main Snap (Mobile ver.)"
-            />
-          ))}
+          <CardMedia
+            className={styles.detailFrameBottom}
+            image={imgDetailTitleFrameBottom}
+          />
+        </div>
 
-          <div className={styles.introduce}>
-            <div className={styles.newlywedsContainer}>
-              <Typography variant='h4' component='span'
-                          className={classNames(styles.newlywedsName, styles.fontHandwriting)}>
-                한터</Typography>
-              <Typography variant='h5' component='span'
-                          className={classNames(styles.newlywedsHeart, styles.fontHandwriting)}>
-                &nbsp;♥&nbsp;</Typography>
-              <Typography variant='h4' component='span'
-                          className={classNames(styles.newlywedsName, styles.fontHandwriting)}>
-                수지</Typography>
-            </div>
-            <div className={styles.dateContainer}>
-              <Typography variant='h6' component='div'
-                          className={classNames(styles.date, styles.fontHandwriting)}>
-                2019.08.07. WED.
-              </Typography>
-            </div>
-          </div>
+        <div className={styles.gallery}>
+          <Typography variant='h3' component='h4' gutterBottom
+                      className={classNames(styles.detailTitle, styles.fontCursive)}>
+            Gallery
+          </Typography>
 
-          <div className={styles.details}>
-            <CardMedia
-              className={styles.detailFrameTop}
-              image={imgDetailTitleFrameTop}
-            />
-            <Typography variant='h3' component='h3' gutterBottom
-                        className={classNames(styles.detailTitle, styles.fontCursive)}>
-              Save the Date
-            </Typography>
-            <Typography variant='p' component='p' paragraph
-                        className={classNames(styles.detailContent, styles.fontSerif)}>
-              서로가 마주보며 다져온 사랑을 <br/>
-              이제 함께 한 곳을 바라보며 걸어갈 수 있는 <br/>
-              큰 사랑으로 키우고자 합니다. <br/>
-              <br/>
-              저희 두 사람이 사랑의 이름으로 지켜나갈 수 있게 <br/>
-              앞날을 축복해 주시면 감사하겠습니다.
-            </Typography>
-            <CardMedia
-              className={styles.detailSubSnap}
-              image={imgSnapSub}
-              title="Sub Snap"
-            />
+          <ImageGallery items={imgSnaps} lazyLoad={false} useBrowserFullscreen={false}
+                        useTranslate3D={false} showPlayButton={false}
+                        preventDefaultTouchmoveEvent={true} />
+        </div>
 
-            <div className={styles.newlywedsContainer}>
-              <Typography variant='h4' component='div'
-                          className={classNames(styles.newlywedsName2, styles.fontSerif)}>
-                정한터&nbsp;</Typography>
-              <CardMedia
-                className={styles.newlywedsFlowerImg}
-                image={imgHeartFlower}
-                title="Heart"
-              />
-              <Typography variant='h4' component='div'
-                          className={classNames(styles.newlywedsName2, styles.fontSerif)}>
-                &nbsp;황수지</Typography>
-            </div>
-
-            <Typography variant='h6' component='div'
-                        className={classNames(styles.detailFamily, styles.fontSerif)}>
-              정문식 · 박희숙의 장남&nbsp;&nbsp;<strong className={styles.detailFamilyNewlywed}>한터</strong>
-              <br/>
-              황용환 · 김윤희의 장녀&nbsp;&nbsp;<strong className={styles.detailFamilyNewlywed}>수지</strong>
-            </Typography>
-
-            <CardMedia
-              className={styles.detailFrameBottom}
-              image={imgDetailTitleFrameBottom}
-            />
-          </div>
-
-          <div className={styles.gallery}>
-            <Typography variant='h3' component='h4' gutterBottom
-                        className={classNames(styles.detailTitle, styles.fontCursive)}>
-              Gallery
-            </Typography>
-
-            <ImageGallery items={imgSnaps} lazyLoad={false} useBrowserFullscreen={false}
-                          useTranslate3D={false} showPlayButton={false}
-                          preventDefaultTouchmoveEvent={true}/>
-          </div>
-
-        </Container>
-      </React.Fragment>
-    );
-  }
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default App;
