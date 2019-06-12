2b53702f34a2fd766cf3eeecc423a05a1d6e834d
Hanter Jung <hanterkr@gmail.com>
2019-06-12 17:27:13 +0900

    FIx images local img to Imgur hosting image

--- src/assets/img/snap/index.js
+++ src/assets/img/snap/index.js
@@ -1,111 +1,111 @@
-import imgSnap01 from './01.jpg';
-import imgSnap02 from './02.jpg';
-import imgSnap03 from './03.jpg';
-import imgSnap04 from './04.jpg';
-import imgSnap05 from './05.jpg';
-import imgSnap06 from './06.jpg';
-import imgSnap07 from './07.jpg';
-import imgSnap08 from './08.jpg';
-import imgSnap09 from './09.jpg';
-import imgSnap10 from './10.jpg';
-import imgSnap11 from './11.jpg';
-import imgSnap12 from './12.jpg';
-import imgSnap13 from './13.jpg';
-import imgSnap14 from './14.jpg';
-import imgSnap15 from './15.jpg';
-import imgSnap16 from './16.jpg';
-import imgSnap17 from './17.jpg';
-import imgSnap18 from './18.jpg';
-import imgSnap19 from './19.jpg';
-import imgSnap20 from './20.jpg';
-import imgSnap21 from './21.jpg';
-import imgSnap22 from './22.jpg';
-import imgSnap23 from './23.jpg';
-import imgSnap24 from './24.jpg';
-import imgSnap25 from './25.jpg';
-import imgSnap26 from './26.jpg';
-import imgSnap27 from './27.jpg';
-import imgSnap28 from './28.jpg';
-import imgSnap29 from './29.jpg';
-import imgSnap30 from './30.jpg';
-import imgSnap31 from './31.jpg';
-import imgSnap32 from './32.jpg';
-import imgSnap33 from './33.jpg';
-import imgSnap34 from './34.jpg';
-import imgSnap35 from './35.jpg';
-
-import thumbSnap01 from './thumbnail/01.jpg';
-import thumbSnap02 from './thumbnail/02.jpg';
-import thumbSnap03 from './thumbnail/03.jpg';
-import thumbSnap04 from './thumbnail/04.jpg';
-import thumbSnap05 from './thumbnail/05.jpg';
-import thumbSnap06 from './thumbnail/06.jpg';
-import thumbSnap07 from './thumbnail/07.jpg';
-import thumbSnap08 from './thumbnail/08.jpg';
-import thumbSnap09 from './thumbnail/09.jpg';
-import thumbSnap10 from './thumbnail/10.jpg';
-import thumbSnap11 from './thumbnail/11.jpg';
-import thumbSnap12 from './thumbnail/12.jpg';
-import thumbSnap13 from './thumbnail/13.jpg';
-import thumbSnap14 from './thumbnail/14.jpg';
-import thumbSnap15 from './thumbnail/15.jpg';
-import thumbSnap16 from './thumbnail/16.jpg';
-import thumbSnap17 from './thumbnail/17.jpg';
-import thumbSnap18 from './thumbnail/18.jpg';
-import thumbSnap19 from './thumbnail/19.jpg';
-import thumbSnap20 from './thumbnail/20.jpg';
-import thumbSnap21 from './thumbnail/21.jpg';
-import thumbSnap22 from './thumbnail/22.jpg';
-import thumbSnap23 from './thumbnail/23.jpg';
-import thumbSnap24 from './thumbnail/24.jpg';
-import thumbSnap25 from './thumbnail/25.jpg';
-import thumbSnap26 from './thumbnail/26.jpg';
-import thumbSnap27 from './thumbnail/27.jpg';
-import thumbSnap28 from './thumbnail/28.jpg';
-import thumbSnap29 from './thumbnail/29.jpg';
-import thumbSnap30 from './thumbnail/30.jpg';
-import thumbSnap31 from './thumbnail/31.jpg';
-import thumbSnap32 from './thumbnail/32.jpg';
-import thumbSnap33 from './thumbnail/33.jpg';
-import thumbSnap34 from './thumbnail/34.jpg';
-import thumbSnap35 from './thumbnail/35.jpg';
+// import imgSnap01 from './01.jpg';
+// import imgSnap02 from './02.jpg';
+// import imgSnap03 from './03.jpg';
+// import imgSnap04 from './04.jpg';
+// import imgSnap05 from './05.jpg';
+// import imgSnap06 from './06.jpg';
+// import imgSnap07 from './07.jpg';
+// import imgSnap08 from './08.jpg';
+// import imgSnap09 from './09.jpg';
+// import imgSnap10 from './10.jpg';
+// import imgSnap11 from './11.jpg';
+// import imgSnap12 from './12.jpg';
+// import imgSnap13 from './13.jpg';
+// import imgSnap14 from './14.jpg';
+// import imgSnap15 from './15.jpg';
+// import imgSnap16 from './16.jpg';
+// import imgSnap17 from './17.jpg';
+// import imgSnap18 from './18.jpg';
+// import imgSnap19 from './19.jpg';
+// import imgSnap20 from './20.jpg';
+// import imgSnap21 from './21.jpg';
+// import imgSnap22 from './22.jpg';
+// import imgSnap23 from './23.jpg';
+// import imgSnap24 from './24.jpg';
+// import imgSnap25 from './25.jpg';
+// import imgSnap26 from './26.jpg';
+// import imgSnap27 from './27.jpg';
+// import imgSnap28 from './28.jpg';
+// import imgSnap29 from './29.jpg';
+// import imgSnap30 from './30.jpg';
+// import imgSnap31 from './31.jpg';
+// import imgSnap32 from './32.jpg';
+// import imgSnap33 from './33.jpg';
+// import imgSnap34 from './34.jpg';
+// import imgSnap35 from './35.jpg';
+//
+// import thumbSnap01 from './thumbnail/01.jpg';
+// import thumbSnap02 from './thumbnail/02.jpg';
+// import thumbSnap03 from './thumbnail/03.jpg';
+// import thumbSnap04 from './thumbnail/04.jpg';
+// import thumbSnap05 from './thumbnail/05.jpg';
+// import thumbSnap06 from './thumbnail/06.jpg';
+// import thumbSnap07 from './thumbnail/07.jpg';
+// import thumbSnap08 from './thumbnail/08.jpg';
+// import thumbSnap09 from './thumbnail/09.jpg';
+// import thumbSnap10 from './thumbnail/10.jpg';
+// import thumbSnap11 from './thumbnail/11.jpg';
+// import thumbSnap12 from './thumbnail/12.jpg';
+// import thumbSnap13 from './thumbnail/13.jpg';
+// import thumbSnap14 from './thumbnail/14.jpg';
+// import thumbSnap15 from './thumbnail/15.jpg';
+// import thumbSnap16 from './thumbnail/16.jpg';
+// import thumbSnap17 from './thumbnail/17.jpg';
+// import thumbSnap18 from './thumbnail/18.jpg';
+// import thumbSnap19 from './thumbnail/19.jpg';
+// import thumbSnap20 from './thumbnail/20.jpg';
+// import thumbSnap21 from './thumbnail/21.jpg';
+// import thumbSnap22 from './thumbnail/22.jpg';
+// import thumbSnap23 from './thumbnail/23.jpg';
+// import thumbSnap24 from './thumbnail/24.jpg';
+// import thumbSnap25 from './thumbnail/25.jpg';
+// import thumbSnap26 from './thumbnail/26.jpg';
+// import thumbSnap27 from './thumbnail/27.jpg';
+// import thumbSnap28 from './thumbnail/28.jpg';
+// import thumbSnap29 from './thumbnail/29.jpg';
+// import thumbSnap30 from './thumbnail/30.jpg';
+// import thumbSnap31 from './thumbnail/31.jpg';
+// import thumbSnap32 from './thumbnail/32.jpg';
+// import thumbSnap33 from './thumbnail/33.jpg';
+// import thumbSnap34 from './thumbnail/34.jpg';
+// import thumbSnap35 from './thumbnail/35.jpg';
 
 const images = [
-  {original: 'https://i.imgur.com/rZEbRv9.jpg', thumbnail:thumbSnap01},
-  {original: 'https://i.imgur.com/wGAgL8d.jpg', thumbnail:thumbSnap02},
-  {original: 'https://i.imgur.com/XIIizrz.jpg', thumbnail:thumbSnap03},
-  {original: imgSnap04, thumbnail:thumbSnap04},
-  {original: imgSnap05, thumbnail:thumbSnap05},
-  {original: imgSnap06, thumbnail:thumbSnap06},
-  {original: imgSnap07, thumbnail:thumbSnap07, originalClass: 'portrait'},
-  {original: imgSnap08, thumbnail:thumbSnap08, originalClass: 'portrait'},
-  {original: imgSnap09, thumbnail:thumbSnap09, originalClass: 'portrait'},
-  {original: imgSnap10, thumbnail:thumbSnap10},
-  {original: imgSnap11, thumbnail:thumbSnap11},
-  {original: imgSnap12, thumbnail:thumbSnap12},
-  {original: imgSnap13, thumbnail:thumbSnap13},
-  {original: imgSnap14, thumbnail:thumbSnap14},
-  {original: imgSnap15, thumbnail:thumbSnap15},
-  {original: imgSnap16, thumbnail:thumbSnap16},
-  {original: imgSnap17, thumbnail:thumbSnap17},
-  {original: imgSnap18, thumbnail:thumbSnap18},
-  {original: imgSnap19, thumbnail:thumbSnap19},
-  {original: imgSnap20, thumbnail:thumbSnap20},
-  {original: imgSnap21, thumbnail:thumbSnap21},
-  {original: imgSnap22, thumbnail:thumbSnap22},
-  {original: imgSnap23, thumbnail:thumbSnap23},
-  {original: imgSnap24, thumbnail:thumbSnap24},
-  {original: imgSnap25, thumbnail:thumbSnap25},
-  {original: imgSnap26, thumbnail:thumbSnap26},
-  {original: imgSnap27, thumbnail:thumbSnap27},
-  {original: imgSnap28, thumbnail:thumbSnap28},
-  {original: imgSnap29, thumbnail:thumbSnap29},
-  {original: imgSnap30, thumbnail:thumbSnap30},
-  {original: imgSnap31, thumbnail:thumbSnap31},
-  {original: imgSnap32, thumbnail:thumbSnap32},
-  {original: imgSnap33, thumbnail:thumbSnap33},
-  {original: imgSnap34, thumbnail:thumbSnap34},
-  {original: imgSnap35, thumbnail:thumbSnap35},
+  {original: 'https://i.imgur.com/rZEbRv9.jpg', thumbnail:'https://i.imgur.com/mNS6wtc.jpg', snapIdx: 1},
+  {original: 'https://i.imgur.com/wGAgL8d.jpg', thumbnail:'https://i.imgur.com/XwgeBxY.jpg', snapIdx: 2},
+  {original: 'https://i.imgur.com/XIIizrz.jpg', thumbnail:'https://i.imgur.com/eVEaII8.jpg', snapIdx: 3},
+  {original: 'https://i.imgur.com/dYqG4lF.jpg', thumbnail:'https://i.imgur.com/GGshmcX.jpg', snapIdx: 4},
+  {original: 'https://i.imgur.com/hnBRNvq.jpg', thumbnail:'https://i.imgur.com/fvTpKMn.jpg', snapIdx: 5},
+  {original: 'https://i.imgur.com/fBtNg6S.jpg', thumbnail:'https://i.imgur.com/ov7l43u.jpg', snapIdx: 6},
+  {original: 'https://i.imgur.com/GJO0rLo.jpg', thumbnail:'https://i.imgur.com/RKosXQe.jpg', snapIdx: 7, originalClass: 'portrait'},
+  {original: 'https://i.imgur.com/gNtZwju.jpg', thumbnail:'https://i.imgur.com/cel2UUc.jpg', snapIdx: 8, originalClass: 'portrait'},
+  {original: 'https://i.imgur.com/hGVWQjh.jpg', thumbnail:'https://i.imgur.com/DjIusFV.jpg', snapIdx: 9, originalClass: 'portrait'},
+  {original: 'https://i.imgur.com/O1tH3Ll.jpg', thumbnail:'https://i.imgur.com/zxKWHoR.jpg', snapIdx: 10},
+  {original: 'https://i.imgur.com/fPQSPHK.jpg', thumbnail:'https://i.imgur.com/bcb4xI0.jpg', snapIdx: 12},
+  {original: 'https://i.imgur.com/rVCxcCo.jpg', thumbnail:'https://i.imgur.com/9YpOiiW.jpg', snapIdx: 13},
+  {original: 'https://i.imgur.com/QfRrMow.jpg', thumbnail:'https://i.imgur.com/ymiEvsj.jpg', snapIdx: 14},
+  {original: 'https://i.imgur.com/BXMVUQJ.jpg', thumbnail:'https://i.imgur.com/KcBYn9V.jpg', snapIdx: 15},
+  {original: 'https://i.imgur.com/pabNNQ6.jpg', thumbnail:'https://i.imgur.com/dhMUCfA.jpg', snapIdx: 16},
+  {original: 'https://i.imgur.com/qid0CQf.jpg', thumbnail:'https://i.imgur.com/SAyPwa4.jpg', snapIdx: 17},
+  {original: 'https://i.imgur.com/MNhkfD5.jpg', thumbnail:'https://i.imgur.com/XqmrjCY.jpg', snapIdx: 18},
+  {original: 'https://i.imgur.com/GMVaewZ.jpg', thumbnail:'https://i.imgur.com/JqX6ia1.jpg', snapIdx: 19},
+  {original: 'https://i.imgur.com/sqfQn9p.jpg', thumbnail:'https://i.imgur.com/MEQa5qw.jpg', snapIdx: 20},
+  {original: 'https://i.imgur.com/zNWD000.jpg', thumbnail:'https://i.imgur.com/am0sL16.jpg', snapIdx: 21},
+  {original: 'https://i.imgur.com/1iCUAVf.jpg', thumbnail:'https://i.imgur.com/NgftAch.jpg', snapIdx: 22},
+  {original: 'https://i.imgur.com/xBegzSY.jpg', thumbnail:'https://i.imgur.com/TKl4cro.jpg', snapIdx: 23},
+  {original: 'https://i.imgur.com/q0KcH2D.jpg', thumbnail:'https://i.imgur.com/yTanjJy.jpg', snapIdx: 24},
+  {original: 'https://i.imgur.com/fRJU8CS.jpg', thumbnail:'https://i.imgur.com/mbxBfoG.jpg', snapIdx: 25},
+  {original: 'https://i.imgur.com/ezinKd7.jpg', thumbnail:'https://i.imgur.com/kIY7OOE.jpg', snapIdx: 26},
+  {original: 'https://i.imgur.com/uA2Hxjx.jpg', thumbnail:'https://i.imgur.com/ZJFricT.jpg', snapIdx: 27},
+  {original: 'https://i.imgur.com/OFDDby7.jpg', thumbnail:'https://i.imgur.com/XHUVRMJ.jpg', snapIdx: 28},
+  {original: 'https://i.imgur.com/1dYPIYK.jpg', thumbnail:'https://i.imgur.com/U98qqKK.jpg', snapIdx: 29},
+  {original: 'https://i.imgur.com/6Eq7CP7.jpg', thumbnail:'https://i.imgur.com/F7V3WXA.jpg', snapIdx: 30},
+  {original: 'https://i.imgur.com/5nefz83.jpg', thumbnail:'https://i.imgur.com/jxw4vIE.jpg', snapIdx: 31},
+  {original: 'https://i.imgur.com/yjRye1a.jpg', thumbnail:'https://i.imgur.com/q4gkXND.jpg', snapIdx: 32},
+  {original: 'https://i.imgur.com/HDzs4Nl.jpg', thumbnail:'https://i.imgur.com/JGRyiF9.jpg', snapIdx: 33},
+  {original: 'https://i.imgur.com/9eWTC8I.jpg', thumbnail:'https://i.imgur.com/cdalzsH.jpg', snapIdx: 34},
+  {original: 'https://i.imgur.com/W95fe2j.jpg', thumbnail:'https://i.imgur.com/HkzEi2E.jpg', snapIdx: 35},
+  {original: 'https://i.imgur.com/v5nwD1r.jpg', thumbnail:'https://i.imgur.com/psmrfVl.jpg', snapIdx: 36},
 ];
 
 export default images;
